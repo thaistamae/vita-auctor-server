89eee1ba1c32d2b1896d6dc8fd082351a47cdd21
Alexandre Lima <alexandreatlima@gmail.com>
2022-02-28 10:20:39 -0300

    sign up done

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,3 +1,36 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
+
+router.post("/signup", async (req, res) => {
+  try {
+    const { password } = req.body;
+
+    if (
+      !password ||
+      !password.match(
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/
+      )
+    ) {
+      return res.status(400).json({
+        msg: "Password is required and must have at least 8 characters, uppercase and lowercase letters, numbers and special characters.",
+      });
+    }
+
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const createdUser = await UserModel.create({
+      ...req.body,
+      passwordHash: hashedPassword,
+    });
+
+    return res.status(201).json(createdUser);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
+  }
+});
 
 module.exports = router;
