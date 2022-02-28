e1b3cbb028e298e837b02db88e5d5b05b8029ba4
Alexandre Lima <alexandreatlima@gmail.com>
2022-02-28 09:50:51 -0300

    user router done

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
-const dbConnection = require("./config/db.config");
-dbConnection();
+require("dotenv").config();
+require("./config/db.config")();
 
 app.use(express.json());
+
+const API_VERSION = "v0";
+
+const userRouter = require("./routes/user.routes");
+app.use(`/api/${API_VERSION}/users`, userRouter);
 
 app.listen(Number(process.env.PORT), () => {
   console.log("Server UP! PORT: ", process.env.PORT);
